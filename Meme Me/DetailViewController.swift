6d5397949b51d9a80c5b9196fd218c650820b1b0
kvo180 <khoavo180@gmail.com>
2015-10-17 03:12:07 -0700

    Add code to allow hide/show of nav and tab bar in Detail View

--- Meme Me/DetailViewController.swift
+++ Meme Me/DetailViewController.swift
@@ -8,15 +8,47 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
     var meme: Meme!
     
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add tap gestures to hide/show navbars
+        let hideShow: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "hideShowToolbars")
+        view.addGestureRecognizer(hideShow)
+        hideShow.delegate = self
+    }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
         imageView.image = meme.memedImage
+    }
+    
+    // Hide/show navbars
+    func hideShowToolbars() {
+        // If toolbars are visible, hide them
+        let navBar = navigationController?.navigationBar
+        let tabBar = tabBarController?.tabBar
+        if navBar?.alpha >= 0.8 && tabBar?.alpha >= 0.8 {
+            UIView.animateWithDuration(0.3, animations: {
+                navBar?.alpha = 0
+                tabBar?.alpha = 0
+            })
+        }
+        else {
+            UIView.animateWithDuration(0.3, animations: {
+                navBar?.alpha = 0.8
+                tabBar?.alpha = 0.8
+            })
+        }
     }
 }
